0d2aae2c80d9f2a9c2ddd921787a846733ee3c7c
cristophercauna <ccaunam@unjbg.edu.pe>
2025-06-18 19:43:41 -0500

    ejemplo.cpp: Imprime el N° de empleado, el nombre, ventas y el salario.

--- ejemplo.cpp
+++ ejemplo.cpp
@@ -12,7 +12,9 @@ EMP EMPLEADOS [100];
 int n;
 cin>>n;
 for(int i=0 ; i<n ; i++){
-    cin>>EMPLEADOS[i].num>>EMPLEADOS[i].nom;
+    cin>>EMPLEADOS[i].num;
+    cin.ignore();
+    getline(cin,EMPLEADOS[i].nom);
     for(int j=0 ; j<12 ; j++)
     cin>>EMPLEADOS[i].ven[j];
     cin>>EMPLEADOS[i].sal;
@@ -21,7 +23,7 @@ for(int i=0 ; i<n ; i++){
     cout<<EMPLEADOS[i].num<<endl;
     cout<<EMPLEADOS[i].nom<<endl;
     for(int j=0 ; j<12 ; j++)
-    cout<<EMPLEADOS[i].ven[j]<<" "<<endl;
+    cout<<EMPLEADOS[i].ven[j]<<" ";
     cout<<EMPLEADOS[i].sal<<endl;
 }
 return 0;

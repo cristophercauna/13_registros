11f15d4b33c73f988b6a37d4d54f579d44202a7a
cristophercauna <ccaunam@unjbg.edu.pe>
2025-06-21 14:59:34 -0500

    Agregar a registros2.cpp :Muestra la cantidad de personas con edades mayores a 50, calcula y muestra el promedio de edades y impreme los datos ingresados.

--- registros2.cpp
+++ registros2.cpp
@@ -18,6 +18,31 @@ int main(){
         cin>>personas[i].DNI;
         cout<<"Persona: "<<i+1<<" -Edad: ";
         cin>>personas[i].edad;
+        cin.ignore();
+    }
+    int sumedades,contador;
+    float promedades;
+    sumedades=0; contador=0; promedades=0;
+    for(i=0;i<n;i++){
+        sumedades=sumedades+personas[i].edad;
+        if(personas[i].edad>50){
+            contador=contador+1;
+        }
+    }
+    if(n>0){
+        promedades=(float)sumedades/n;
+    }
+    cout<<"Cantidad de personas mayores a 50: "<<contador<<endl;
+    cout<<"\n Promedio de edades: "<<promedades<<endl;
+    
+    cout<<"\n======= DATOS DE PERSONAS INGRESADAS =======\n";
+    for(i=0;i<n;i++){
+        cout<<"Persona: "<<i+1<<" -Nombre: ";
+        cout<<personas[i].nom<<endl;
+        cout<<"Persona: "<<i+1<<" -DNI: ";
+        cout<<personas[i].DNI<<endl;
+        cout<<"Persona: "<<i+1<<" -Edad: ";
+        cout<<personas[i].edad<<endl;
     }
     return 0;
 }

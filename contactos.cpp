1497009c30c6b34bbcefdfceb51d8fc1cc6892c7
cristophercauna <ccaunam@unjbg.edu.pe>
2025-06-25 20:08:48 -0500

    Agregar acontactos.cpp: Modifica los contactos ya registrados y pregunta si desea salir del programa.

--- contactos.cpp
+++ contactos.cpp
@@ -19,7 +19,7 @@ void leerContacto(contactoEmail &, string, char, int, correo);
 void imprimeContacto(contactoEmail &);
 
 int main(){
-    int n, op;
+    int n,k,op;
     string nom, user, domain;
     char sex;
     int edad;
@@ -30,7 +30,8 @@ int main(){
         system("cls");
         cout<<"Menu de opciones -------------------------"<<endl;
         cout<<"1. Agregar contacto"<<endl;
-        cout<<"2. Mostrar contactos"<<endl;
+        cout<<"2. Modificar contactos"<<endl;
+        cout<<"3. Mostrar contactos"<<endl;
         cout<<"0. Salir"<<endl;
         cout<<"Elige una opcion: "; cin>>op;
         switch(op){
@@ -53,6 +54,35 @@ int main(){
                 system("pause");
                 break;
             case 2:
+                if(n==0){
+                    cout<<"No hay contactos que modificar"<<endl;
+                }
+                cout<<"Contactos Disponibles"<<endl;
+                for(int i=0;i<n;i++){
+                    cout<<"Contacto #"<<i+1<<":"<<lista[i].nom<<endl;
+                }
+                cout<<"Ingrese el contacto que quiere modificar"; cin>>k;
+                k=k-1;
+                if(k<0 ||k>=n){
+                    cout<<"El contacto a modificar no esta registrado"<<endl;
+                    system("pause");
+                    break;
+                }
+                cout<<"Contacto a modificar #"<<endl;
+                imprimeContacto(lista[k]);
+                cout<<"-----------------------------------"<<endl;
+                cout<<"Apartir de aca los datos que ingresen seran los nuevos"<<endl;
+                cin.ignore();
+                cout<<"Nombre | "; getline(cin,nom);
+                cout<<"Sexo | "; cin>>sex;
+                cout<<"Edad | "; cin>>edad;
+                cout<<"Ingrese el Correo Electronico (usurio@dominio)"<<endl;
+                cout<<"\tUsuario | "; cin>>user;
+                cout<<"\tDominio | "; cin>>domain;
+                leerCorreo(email,user,domain);
+                leerContacto(lista[k],nom,sex,edad,email);
+                break;
+            case 3:
                 for(int i = 0; i < n; i++){
                     cout<<"Contacto #"<<i+1<<endl;
                     imprimeContacto(lista[i]);
@@ -61,8 +91,20 @@ int main(){
                 system("pause");
                 break;
             case 0:
+                char preg;
                 cout<<"Esta seguro de salir? (S/N): ";
+                cin>>preg;
+                if(preg=='S' || preg=='s'){
+                    cout<<"El programa a terminado, adios"<<endl;
+                }else{
+                    if(preg=='N' || preg=='n'){
+                    cout<<"Volviendo al menu"<<endl;
+                    system("pause");
+                    op=1;
+                    } 
+                }
                 break;
+            
             default:
                 cout<<"Opcion no valida!"<<endl;
                 system("pause");

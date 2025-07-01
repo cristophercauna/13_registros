4ed52fb18a4e3c542ab98f1455712f72d8f2a1f2
cristophercauna <ccaunam@unjbg.edu.pe>
2025-07-01 17:45:46 -0500

    Agregamos archivo contacto.h con las estructuras

---
+++ Contactos/contacto.h
@@ -0,0 +1,17 @@
+#ifndef CONTACTO.H
+#define CONTACTO.H
+#include<string>
+using namespace std;
+struct correo{
+    string user;
+    string domain;
+};
+
+struct contactoEmail{
+    string nom;
+    char sex;
+    int edad;
+    correo email;
+};
+
+#endif

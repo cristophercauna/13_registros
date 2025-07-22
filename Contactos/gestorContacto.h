0ee57b690e3ff1b2a7d090ed2778adc4a3992ec8
cristophercauna <ccaunam@unjbg.edu.pe>
2025-07-21 20:30:30 -0500

    Actualizar gestorContacto.h : Se añade el prototipo de la funcion eliminarContacto.

--- Contactos/gestorContacto.h
+++ Contactos/gestorContacto.h
@@ -10,5 +10,6 @@ void agregarContacto(contactoEmail lista[], int &n, contactoEmail nuevo);
 void modificarContacto(contactoEmail lista[],int n, int k, contactoEmail nuevo);
 void mostrarContactos(contactoEmail lista[], int n);
 void buscarPorDominio(contactoEmail lista[], int n,string dominio);
+void eliminarContacto(contactoEmail lista[], int &n, int indix);
 
 #endif
